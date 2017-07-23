41a2c89ea7628968ff537464bcf6262aa3cc3c3d
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-07-23 20:55:27 +0000

    initial model for testing

--- clone.py
+++ clone.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Input, Flatten, Dense
 
 
 lines = []
@@ -17,7 +17,7 @@ measurements = []
 for line in lines:
     source_path = line[0]
     file_name = source_path.split('/')[-1]
-    current_path = './data/IMG/' + file_name
+    current_path = './data/set2/IMG/' + file_name
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -27,10 +27,13 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 model = Sequential()
+input_shape = X_train.shape[1:]
+#inp = Input(shape=input_shape)
+#model.add(Flatten(input_shape=input_shape))
 model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
