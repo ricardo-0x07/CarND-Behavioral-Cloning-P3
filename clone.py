6d17dcc394deed22f37be70e85e91d87f0b12b1f
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-07-25 06:26:08 +0000

    augmented data by flipping images about thier y axis and inverted steering angles to offset models tendedncy to vear left.

--- clone.py
+++ clone.py
@@ -24,8 +24,16 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images = []
+augmented_measurements = []
+for image, measurment in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160,320,3)))
