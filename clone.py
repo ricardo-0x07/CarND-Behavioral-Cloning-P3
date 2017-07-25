66d8595ba94927957bc37cbbd13ada9882e776ef
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-07-25 21:10:42 +0000

    increased epochs from 5 to 10

--- clone.py
+++ clone.py
@@ -61,6 +61,6 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
 model.save('model.h5')
