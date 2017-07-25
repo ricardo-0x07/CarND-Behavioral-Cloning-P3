4fe4049f9acd2d85387595502ec498c49d8f0e4b
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-07-25 02:54:12 +0000

    implemented normalization

--- clone.py
+++ clone.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Input, Flatten, Dense
+from keras.layers import Input, Flatten, Dense, Lambda
 
 
 lines = []
@@ -27,13 +27,12 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 model = Sequential()
-input_shape = X_train.shape[1:]
-#inp = Input(shape=input_shape)
-#model.add(Flatten(input_shape=input_shape))
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160,320,3)))
+
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 
 model.save('model.h5')
