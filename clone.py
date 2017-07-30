c65f3663d0dfb4a41017bac8817feccc919e7e94
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-07-30 21:59:38 +0000

    addtional data of smooth driving

--- clone.py
+++ clone.py
@@ -1,3 +1,4 @@
+import itertools
 import csv
 import cv2
 import numpy as np
@@ -12,7 +13,8 @@ images = []
 measurements = []
 with open('./data/set3/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    for line in reader:
+    for line in itertools.islice(reader, 0, 19000):
+        #print('line: ', line)
         steering_center = float(line[3])
         # create adjusted steering measurements for the side camera images
         correction = 0.3 # this is a parameter to tune
@@ -96,6 +98,6 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.summary()
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, batch_size=128, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 model.save('model.h5')
