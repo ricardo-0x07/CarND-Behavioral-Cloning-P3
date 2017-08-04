ae91d3278ba23076d0c99841bd176cce5d183dcf
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-08-04 04:24:29 +0000

    fix: ajusted cf .2 and included more data

--- clone.py
+++ clone.py
@@ -25,11 +25,11 @@ def resize_function(image):
 
 with open('./data/set3/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    for line in itertools.islice(reader, 0, 19000):
+    for line in itertools.islice(reader, 0, 33000):
         #print('line: ', line)
         steering_center = float(line[3])
         # create adjusted steering measurements for the side camera images
-        correction = 0.1 # this is a parameter to tune
+        correction = 0.2 # this is a parameter to tune
         steering_left = steering_center + correction
         steering_right = steering_center - correction
         # read in images from center, left and right cameras
