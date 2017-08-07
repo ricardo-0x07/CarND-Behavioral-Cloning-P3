5768170b607d783f48c7dc0b006ccb186417380d
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-08-07 17:48:36 +0000

    fix: increased dropout adjusted cf 0.2

--- clone.py
+++ clone.py
@@ -39,7 +39,7 @@ with open('./data/set3/driving_log.csv') as csvfile:
       #print('line: ', line)
         steering_center = float(line[3])
         # create adjusted steering measurements for the side camera images
-        correction = 0.0436 # this is a parameter to tune
+        correction = 0.2 # this is a parameter to tune
         steering_left = steering_center + correction
         steering_right = steering_center - correction
         # read in images from center, left and right cameras
@@ -81,25 +81,25 @@ model.add(Conv2D(36, (5, 5), strides=1, padding='same'))
 model.add(BatchNormalization(axis=1))
 model.add(Activation('elu'))
 model.add(MaxPooling2D())
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(48, (5, 5), strides=1, padding='same'))  
 model.add(BatchNormalization(axis=1))
 model.add(Activation('elu'))
 model.add(MaxPooling2D())
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same'))  
 model.add(BatchNormalization(axis=1))
 model.add(Activation('elu'))
 model.add(MaxPooling2D())
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same'))  
 model.add(BatchNormalization(axis=1))
 model.add(Activation('elu'))
 model.add(MaxPooling2D())
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 #model.summary()
 
