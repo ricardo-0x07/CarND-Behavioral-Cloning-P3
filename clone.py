5ecd24fbbc9556c1c37c726da353bd91f2856d13
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-08-07 18:55:32 +0000

    fix: epochs 3

--- clone.py
+++ clone.py
@@ -128,6 +128,6 @@ model.add(Dropout(0.25))
 model.add(Dense(1))
 model.summary()
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, batch_size=128, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, batch_size=128, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
