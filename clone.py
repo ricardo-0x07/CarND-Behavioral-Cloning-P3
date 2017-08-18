78d2a5a8742c3ab7d582401b2d2499adba9dc021
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2017-08-18 17:45:12 +0000

    fix: djusted data filter 50%

--- clone.py
+++ clone.py
@@ -36,7 +36,7 @@ with open('./data/set3/driving_log.csv') as csvfile:
     for line in reader:
         if float(line[3]) <= 0.0185:
             low_steer += 1
-            if (low_steer % 3) == 0:
+            if (low_steer % 2) == 0:
                 steerings.append(line)
         else:
             steerings.append(line)
